667a9fac19af0a550a2c0d43bc11d6318bb4c46f
isaac <isaac@rubenfamily.com>
2021-05-17 10:34:21 -0700

    jsx rewrite

--- index.jsx
+++ index.jsx
@@ -8,39 +8,31 @@ const e = React.createElement
 
 const Text = findByDisplayName("Text"),
     Markdown = findByDisplayName("Markdown"),
-    ButtonColors = findByProps("button", "colorRed");
+    ButtonColors = findByProps("button", "colorRed"),
+    ConfirmModal = findByDisplayName("ConfirmModal")
 
-const uploadWindow = srcProps => {
+const makeUploadWindow = srcProps => {
     (0, findByProps("openModal").openModal)((model) => {
-        //if (modal.transitionState === 3) res(false); // If clicked off
 
-        let description = "this is to prevent crash"
-
-        return e(findByDisplayName("ConfirmModal"),
-            {
-                header: "upload",
-                confirmText: "Upload and send link",
-                cancelText: findByProps("Messages").Messages.CANCEL,
-                confirmButtonColor: ButtonColors[`colorRed`],
-                onClose: () => { // General close (?)
-                    console.log("closed")
-                },
-                onCancel: () => { // Cancel text
-                    console.log("canceled")
-                    model.onClose();
-                },
-                onConfirm: () => { // Confirm button
-                    console.log("do it")
-                    model.onClose();
-                },
-                transitionState: model.transitionState,
-            },
-            ...description.split('\n').map((x) => e(Markdown,
-                {
-                    size: Text.Sizes.SIZE_16
-                },
-                x))
-        );
+        return <ConfirmModal
+            header="upload"
+            confirmText="Upload and Send Link"
+            cancelText={findByProps("Messages").Messages.CANCEL}
+            onClose={() => { // General close (?)
+                console.log("closed")
+            }}
+            onCancel={() => { // Cancel text
+                console.log("canceled")
+                model.onClose();
+            }}
+            onConfirm={() => { // Confirm button
+                console.log("do it")
+                model.onClose();
+            }}
+            transitionState={model.transitionState}
+        >
+            <Markdown>test</Markdown>
+        </ConfirmModal>
     });
 }
 
@@ -49,11 +41,19 @@ export default {
         onImport: () => {
             style = document.createElement("style")
             document.head.appendChild(style)
-            //https://github.com/GooseMod/MS2Porter/blob/main/modules/deNitro/index.js
-            style.appendChild(document.createTextNode(".buttons-3JBrkn > button { display: none; }"))
+
+            style.appendChild( //https://github.com/GooseMod/MS2Porter/blob/main/modules/deNitro/index.js
+                document.createTextNode(
+                    ".buttons-3JBrkn > button { display: none; }"
+                )
+            )
         },
         onLoadingFinished: () => {
-            channelTextAreaButtons.patch("upload", "https://avatars.githubusercontent.com/u/19228318?s=48&v=4", uploadWindow)
+            channelTextAreaButtons.patch(
+                "upload",
+                "https://avatars.githubusercontent.com/u/19228318?s=48&v=4",
+                makeUploadWindow
+            )
         },
         onRemove: () => {
             style.remove()
