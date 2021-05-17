20825ed08f2eeb7eb74693047de3edc6ff9a9437
isaac <isaac@rubenfamily.com>
2021-05-16 17:03:39 -0700

    add text area button, remove gift button

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+import { channelTextAreaButtons } from "@goosemod/patcher"
+
+let style
+
 export default {
     goosemodHandlers: {
         onImport: () => {
-            //
+            style = document.createElement("style")
+            document.head.appendChild(style)
+            //https://github.com/GooseMod/MS2Porter/blob/main/modules/deNitro/index.js
+            style.appendChild(document.createTextNode(".buttons-3JBrkn > button { display: none; }"))
         },
         onLoadingFinished: () => {
-            //
+            channelTextAreaButtons.patch("upload", "https://avatars.githubusercontent.com/u/19228318?s=48&v=4", console.log)
         },
         onRemove: () => {
-            //
+            style.remove()
         },
         //this part makes persistent settings work
         // getSettings: () => [settings],
