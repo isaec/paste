92edb24c8ff5aa0b48d05ca93c8dd7fd62eda2d0
isaac <isaac@rubenfamily.com>
2021-05-20 12:32:21 -0700

    really good bash script, stop rebooting if repeat crash

--- dev.sh
+++ dev.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 
 name="${PWD##*/}"
+crashes=0
+lastCrash=$EPOCHSECONDS
 
-cd ../MS2Builder/
+[ ! -d "../MS2Builder/" ] && echo "MS2Builder does not exist at the same level as your folder"
+cd ../MS2Builder/ || exit 1
 
 until node src/dev.js ../$name/ 8000; do
-    echo "MS2 crashed with exit code $?.  Respawning..." >&2
-    sleep 1
+    # this code will be run on crash
+    if ((lastCrash + 5 <= EPOCHSECONDS)); then
+        crashes=1
+    else
+        ((++crashes))
+    fi
+    lastCrash=$EPOCHSECONDS
+    if [[ $crashes -ge 5 ]]; then
+        echo "MS2 crashed [${crashes}x] with exit code $?. It seems to be crashing a lot, check your syntax?" >&2
+        read -r -n 1 -p "Press any key to respawn: "
+        crashes=1
+    else
+        echo "MS2 crashed [${crashes}x] with exit code $?.  Respawning..." >&2
+        sleep 1
+    fi
 done
