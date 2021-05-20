cc33b8ecbf47aede3b6d785ab239e2de78f263a9
isaac <isaac@rubenfamily.com>
2021-05-20 11:51:39 -0700

    autoreboot script, make life easy

--- dev.sh
+++ dev.sh
@@ -3,4 +3,8 @@
 name="${PWD##*/}"
 
 cd ../MS2Builder/
-node src/dev.js ../$name/ 8000
+
+until node src/dev.js ../$name/ 8000; do
+    echo "MS2 crashed with exit code $?.  Respawning..." >&2
+    sleep 1
+done
