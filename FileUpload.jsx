0a2d41d2f30c2c00f864aa7a3c3dd29847260b6c
isaac <isaac@rubenfamily.com>
2021-05-20 19:51:25 -0700

    break file drag into its own element

--- FileUpload.jsx
+++ FileUpload.jsx
@@ -8,38 +8,21 @@ const { useState, useRef } = React
 //     ConfirmModal = findByDisplayName("ConfirmModal"),
 //     Button = findByDisplayName("Button")
 
-const FileUpload = ({
-    updateFilesCb,
-    ...otherProps
-}) => {
-    const fileInputField = useRef(null)
+const FileUpload = props => {
     const [files, setFiles] = useState({})
-    const [dragging, setDragging] = useState(0)
 
-    return <div
-        className="FileUpload"
-    >
+    return <div className="FileUpload" >
         <input
             className="fileUploadInput"
             type="file" name="fileUploadInput"
             id="fileUploadInput"
-            ref={fileInputField}
             multiple="true"
-            {...otherProps}
         />
         <label
             className="fileUploadLabel"
             for="fileUploadInput"
-
-            dragging={dragging}
-
-            onDragEnter={() => setDragging(1)}
-            onDragLeave={() => setDragging(0)}
-            onDrop={() => {
-                console.warn("dropped")
-                setDragging(0)
-            }}
-        >drag and drop, or click to upload files</label>
+        >click to upload files</label>
+        <div className="fileDragZone"></div>
     </div>
 }
 
