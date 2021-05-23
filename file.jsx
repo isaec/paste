bd2227989c1da5dd708ab70d264a00deb8199141
isaac <isaac@rubenfamily.com>
2021-05-22 21:53:12 -0700

    improve file

--- file.jsx
+++ file.jsx
@@ -1,13 +1,21 @@
 const { React } = goosemodScope.webpackModules.common
 const { useState, useRef, useEffect } = React
 
-const File = props => {
+const File = props => <div
+    className="File">
+    <div
+        className="fileText"
+    >{props.name}</div>
+    <div
+        className="fileText"
+    >{props.url || "~"}</div>
+    <div
+        className="fileText"
+    >{props.location}</div>
+    <div
+        className="fileText"
+    >{props.size}</div>
+</div>
 
-
-
-    return <div className="File">
-
-    </div>
-}
 
 export default File
