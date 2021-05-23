55fb24e0b73ed28034fb1c42424cbc9607a0c02c
isaac <isaac@rubenfamily.com>
2021-05-23 14:22:56 -0700

    use img tag to scale file icon properly

--- file.jsx
+++ file.jsx
@@ -5,7 +5,7 @@ const { useState, useRef, useEffect } = React
 
 const File = React.memo(props => <div
     className="File">
-    <div className="fileIcon" />
+    <img src="https://discord.com/assets/66084381f55f4238d69e5cbe3b8dc42e.svg" alt="file icon" className="fileIcon" />
     <div className="fileText"
     >{props.name}</div>
     <div className="fileText"
